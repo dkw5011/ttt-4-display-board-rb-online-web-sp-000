a381095e79ae877aaab78ae91fa152ccac038339
Danielle Bergbower <dkw5011@gmail.com>
2019-03-05 23:24:11 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+
+board=[" "," "," "," ","X"," "," "," "," "]
+display_board(board)
+end
+
+
+
+
+
